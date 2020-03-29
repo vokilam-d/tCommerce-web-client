c1053e5d7f24b415d7874a5abdf961fa597311ae
Malikov Denis <ryki.admirala@gmail.com>
2020-03-29 13:26:12 +0300

    add host for server for docker

--- server.ts
+++ server.ts
@@ -36,7 +36,7 @@ export function app() {
 }
 
 function run() {
-  const port = process.env.PORT || 3002;
+  const port = +process.env.PORT || 3002;
 
   // Start up the Node server
   const server = app();
