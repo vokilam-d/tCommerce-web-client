415cf9dd13379b1b4ba305772a029f2773c0f340
Malikov Denis <ryki.admirala@gmail.com>
2020-03-31 23:26:20 +0300

    update build-push-docker.sh

--- build-push-docker.sh
+++ build-push-docker.sh
@@ -1,2 +1,18 @@
-docker build -t vokilamd/tcommerce-web-client:latest .
-docker push vokilamd/tcommerce-web-client:latest
+CI_REGISTRY_IMAGE=vokilamd/tcommerce-web-client
+
+docker pull $CI_REGISTRY_IMAGE:builder
+docker pull $CI_REGISTRY_IMAGE:latest
+
+docker build \
+  --target builder \
+  --cache-from $CI_REGISTRY_IMAGE:builder \
+  -t $CI_REGISTRY_IMAGE:builder \
+  .
+docker build \
+  --cache-from $CI_REGISTRY_IMAGE:latest \
+  --cache-from $CI_REGISTRY_IMAGE:builder \
+  -t $CI_REGISTRY_IMAGE:latest \
+  .
+
+docker push $CI_REGISTRY_IMAGE:builder
+docker push $CI_REGISTRY_IMAGE:latest
