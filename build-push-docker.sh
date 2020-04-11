0c04e975ad6fdfc2682cce54c8609e996b3dd119
Malikov Denis <ryki.admirala@gmail.com>
2020-04-11 23:51:11 +0300

    fix in build-push-docker.sh

--- build-push-docker.sh
+++ build-push-docker.sh
@@ -1,18 +1,18 @@
 CI_REGISTRY_IMAGE=vokilamd/tcommerce-web-client
 
-docker pull $CI_REGISTRY_IMAGE:builder
-docker pull $CI_REGISTRY_IMAGE:latest
+docker pull ${CI_REGISTRY_IMAGE}:builder
+docker pull ${CI_REGISTRY_IMAGE}:latest
 
 docker build \
   --target builder \
-  --cache-from $CI_REGISTRY_IMAGE:builder \
-  -t $CI_REGISTRY_IMAGE:builder \
+  --cache-from ${CI_REGISTRY_IMAGE}:builder \
+  -t ${CI_REGISTRY_IMAGE}:builder \
   .
 docker build \
-  --cache-from $CI_REGISTRY_IMAGE:latest \
-  --cache-from $CI_REGISTRY_IMAGE:builder \
-  -t $CI_REGISTRY_IMAGE:latest \
+  --cache-from ${CI_REGISTRY_IMAGE}:latest \
+  --cache-from ${CI_REGISTRY_IMAGE}:builder \
+  -t ${CI_REGISTRY_IMAGE}:latest \
   .
 
-docker push $CI_REGISTRY_IMAGE:builder
-docker push $CI_REGISTRY_IMAGE:latest
+docker push ${CI_REGISTRY_IMAGE}:builder
+docker push ${CI_REGISTRY_IMAGE}:latest
