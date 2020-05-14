84bbee9d2d9c068f134ee72d975ee61edf434bb7
Malikov Denis <ryki.admirala@gmail.com>
2020-05-14 19:47:39 +0300

    add Account Discount page

--- src/app/pages/account/account-discount/account-discount.component.ts
+++ src/app/pages/account/account-discount/account-discount.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeadService } from '../../../shared/services/head/head.service';
+import { CustomerService } from '../../../shared/services/customer/customer.service';
+import { DetailedCustomerDto } from '../../../shared/dtos/detailed-customer.dto';
 
 @Component({
   selector: 'account-discount',
@@ -8,7 +10,12 @@ import { HeadService } from '../../../shared/services/head/head.service';
 })
 export class AccountDiscountComponent implements OnInit {
 
-  constructor(private headService: HeadService) { }
+  get totalOrders() { return (this.customerService.customer as DetailedCustomerDto).totalOrdersCount; }
+  get totalCost() { return (this.customerService.customer as DetailedCustomerDto).totalOrdersCost; }
+
+  constructor(private headService: HeadService,
+              private customerService: CustomerService) {
+  }
 
   ngOnInit(): void {
     this.setMeta();
--- src/app/pages/account/account-routing.module.ts
+++ src/app/pages/account/account-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
       { path: 'orders', component: AccountOrdersComponent, data: { label: 'Мои заказы' } },
       // { path: 'wishlist', component: WishlistComponent, data: { label: 'Список желаний' } },
       { path: 'addresses', component: AddressesComponent, data: { label: 'Адреса доставки' } },
-      { path: 'reviews', component: AccountReviewsComponent, data: { label: 'Мои отзывы' } },
+      // { path: 'reviews', component: AccountReviewsComponent, data: { label: 'Мои отзывы' } },
       { path: 'discount', component: AccountDiscountComponent, data: { label: 'Скидки' } }
     ]
   }
