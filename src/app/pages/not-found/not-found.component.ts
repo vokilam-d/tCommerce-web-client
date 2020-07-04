6a2ea58998ae9710dad70440052d76984e172df7
Malikov Denis <ryki.admirala@gmail.com>
2020-07-04 23:10:01 +0300

    set 404 status in not found page

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, Optional } from '@angular/core';
 import { IBreadcrumb } from '../../breadcrumbs/breadcrumbs.interface';
 import { HeadService } from '../../shared/services/head/head.service';
+import { Response } from 'express';
+import { RESPONSE } from '@nguniversal/express-engine/tokens';
+import { DeviceService } from '../../shared/services/device-detector/device.service';
 
 @Component({
   selector: 'not-found',
@@ -11,11 +14,16 @@ export class NotFoundComponent implements OnInit {
 
   breadcrumbs: IBreadcrumb[] = [{ title: '404' }];
 
-  constructor(private headService: HeadService) {
+  constructor(private headService: HeadService,
+              private deviceService: DeviceService,
+              @Optional() @Inject(RESPONSE) private res: Response) {
   }
 
   ngOnInit() {
     this.setMeta();
+    if (this.deviceService.isPlatformServer()) {
+      this.res.status(404);
+    }
   }
 
   private setMeta() {
