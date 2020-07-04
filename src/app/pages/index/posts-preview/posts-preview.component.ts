97c2277cc7f7ffcfb3121cdf4e3f0d5cbcca4ba6
Nastiia <lanastasialysenkol@gmail.com>
2020-07-04 19:13:17 +0300

    add posts-preview component

--- src/app/pages/index/posts-preview/posts-preview.component.ts
+++ src/app/pages/index/posts-preview/posts-preview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { BlogService } from '../../../shared/services/blog/blog.service';
 import { BlogPostListItemDto } from '../../../shared/dtos/blog-post-list-item.dto';
 import { API_HOST } from '../../../shared/constants';
@@ -9,7 +9,13 @@ import { API_HOST } from '../../../shared/constants';
   styleUrls: ['./posts-preview.component.scss']
 })
 export class PostsPreviewComponent implements OnInit {
+  // Din'kin code
+  // posts: BlogPostListItemDto[];
+  //to fetch different categories
+  @Input() category: string
   posts: BlogPostListItemDto[];
+  title: string;
+
   uploadedHost = API_HOST;
 
   constructor(private blogService: BlogService) { }
@@ -18,9 +24,31 @@ export class PostsPreviewComponent implements OnInit {
     this.fetchPosts();
   }
 
+  // Din'kin code
+  // fetchPosts() {
+  //   this.blogService.getLastPosts()
+  //     .subscribe(response => this.posts = response.data);
+  // }
+
+  //to fetch different categories
   fetchPosts() {
-    this.blogService.getLastPosts()
-      .subscribe(response => this.posts = response.data);
+
+    switch (this.category) {
+      case 'lastWorks': {
+        return this.blogService.getPostsList({categoryId: 3, limit: 3})
+          .subscribe(response => {
+            this.posts = response.data;
+            this.title = this.posts[0].category.name;
+          });
+      }
+      case 'lastPosts': {
+        return this.blogService.getPostsList({categoryId: 5, limit: 3})
+          .subscribe(response => {
+            this.posts = response.data;
+            this.title = this.posts[0].category.name;
+          });
+      }
+    }
   }
 
 }
