0666bc84d2d8ab431174b2122d6ad4b379456854
Nastiia <lanastasialysenkol@gmail.com>
2020-08-28 23:03:36 +0300

    [KL-316] remove redundant code

--- src/app/shared/dtos/product-list-response.dto.ts
+++ src/app/shared/dtos/product-list-response.dto.ts
@@ -1,7 +1,6 @@
 import { ResponseDto } from './response.dto';
 import { ProductListItemDto } from './product-list-item.dto';
 import { FilterDto } from './filter.dto';
-import { FilterCategoryDto } from './filter-category.dto';
 
 export class ProductListResponseDto extends ResponseDto<ProductListItemDto[]> {
   filters: FilterDto[];
