8dc0991290de309169adf6da1c08c1848bd0e89a
Nastiia <lanastasialysenkol@gmail.com>
2020-11-18 22:34:31 +0200

    KL-282 add default cities

--- src/app/select-autocomplete/select-autocomplete.component.ts
+++ src/app/select-autocomplete/select-autocomplete.component.ts
@@ -11,8 +11,17 @@ import {
   ViewChild
 } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
-import { fromEvent, Observable, of } from 'rxjs';
-import { catchError, debounceTime, distinctUntilChanged, map, switchMap, takeUntil, tap } from 'rxjs/operators';
+import { EMPTY, fromEvent, Observable, Subscription } from 'rxjs';
+import {
+  catchError,
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  startWith,
+  switchMap,
+  takeUntil,
+  tap
+} from 'rxjs/operators';
 import { ResponseDto } from '../shared/dtos/response.dto';
 import { NgUnsubscribe } from '../shared/directives/ng-unsubscribe.directive';
 import { AddressService } from '../services/address/address.service';
@@ -40,6 +49,7 @@ export class SelectAutocompleteComponent extends NgUnsubscribe implements AfterV
   isVisible: boolean = false;
   options: ISelectOption[] = [];
   private value: any;
+  private inputSubscription: Subscription;
 
   @Input() activeOptionLabel: string;
   @Input() isDisabled: boolean = false;
@@ -58,8 +68,6 @@ export class SelectAutocompleteComponent extends NgUnsubscribe implements AfterV
     if (!this.type) {
       throw new Error('"type" is not provided!');
     }
-
-    this.listenForInput();
   }
 
   onChange = (_: any) => {};
@@ -95,28 +103,33 @@ export class SelectAutocompleteComponent extends NgUnsubscribe implements AfterV
     this.isVisible = isVisible;
     if (this.isVisible) {
       setTimeout(() => this.inputRef.nativeElement.focus(), 50);
+      this.listenForInput();
+    } else {
+      this.inputSubscription?.unsubscribe();
     }
   }
 
   private listenForInput() {
-    fromEvent(this.inputRef.nativeElement, 'input')
+    this.inputSubscription = fromEvent(this.inputRef.nativeElement, 'input')
       .pipe(
         takeUntil(this.ngUnsubscribe),
         debounceTime(200),
         distinctUntilChanged(),
         map((event: InputEvent) => (event.target as HTMLInputElement).value.trim()),
+        startWith(''),
         tap(() => {
           this.isSearchInProgress = true;
           this.cdr.markForCheck();
         }),
-        switchMap(query => this.getRequestByQuery(query)),
-        catchError((err, caught) => {
-          this.searchError = err.error?.message || DEFAULT_ERROR_TEXT;
-          console.error(err);
-          this.isSearchInProgress = false;
-          this.cdr.markForCheck();
-          return caught;
-        })
+        switchMap(query => this.getRequestByQuery(query).pipe(
+          catchError((err, caught) => {
+            this.searchError = err.error?.message || DEFAULT_ERROR_TEXT;
+            console.error(err);
+            this.isSearchInProgress = false;
+            this.cdr.markForCheck();
+            return EMPTY;
+          })
+        ))
       )
       .subscribe(
         response => {
@@ -129,10 +142,6 @@ export class SelectAutocompleteComponent extends NgUnsubscribe implements AfterV
   }
 
   private getRequestByQuery(query: string): Observable<ResponseDto<any>> {
-    if (!query) {
-      return of({ data: [] });
-    }
-
     switch (this.type) {
       case 'settlement':
         return this.addressService.fetchSettlements(query);
