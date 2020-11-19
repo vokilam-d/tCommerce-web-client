52393193308403492d6201cc4c864a488e80c928
Malikov Denis <ryki.admirala@gmail.com>
2020-11-19 17:47:21 +0200

    [KL-286] add product variants

--- src/app/shared/dtos/product-variant.dto.ts
+++ src/app/shared/dtos/product-variant.dto.ts
@@ -2,9 +2,11 @@ export class ProductListItemVariantDto {
   slug: string;
   label: string;
   isSelected: boolean;
+  isInStock: boolean;
 }
 
 export class ProductVariantGroupDto {
   label: string;
+  selectedVariantLabel: string;
   variants: ProductListItemVariantDto[];
 }
