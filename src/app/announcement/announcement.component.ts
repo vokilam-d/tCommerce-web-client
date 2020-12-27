f93aed60334fca2f9b685fc0591b8037d5bdac2e
Nastiia <lanastasialysenkol@gmail.com>
2020-12-27 23:38:55 +0200

    [KL-411] delete container

--- src/app/announcement/announcement.component.ts
+++ src/app/announcement/announcement.component.ts
@@ -9,12 +9,13 @@ import { DEFAULT_ERROR_TEXT } from '../shared/constants';
   styleUrls: ['./announcement.component.scss']
 })
 export class AnnouncementComponent implements OnInit {
-  @ViewChild('announcementRef', { read: ElementRef }) announcementEl: ElementRef;
   public announcementHeight: number;
   public announcement: AnnouncementDto;
   public fetchError: string | null = null;
 
   public type: 'winterHolidays' | 'springHolidays' | 'attention';
+
+  @ViewChild('announcementRef', { read: ElementRef }) announcementEl: ElementRef;
 
   constructor(
     private announcementService: AnnouncementService,
