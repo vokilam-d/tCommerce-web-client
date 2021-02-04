5a9291c2a4119568e70e093900097ccb3a91c889
Nastiia <lanastasialysenkol@gmail.com>
2021-02-04 23:16:31 +0200

    KL-455 remove log

--- src/app/product-list-item/product-list-item.component.ts
+++ src/app/product-list-item/product-list-item.component.ts
@@ -25,7 +25,6 @@ export class ProductListItemComponent implements OnInit, OnChanges {
   ) { }
 
   ngOnInit() {
-    console.log(this.item.label);
   }
 
   ngOnChanges(changes: SimpleChanges): void {
