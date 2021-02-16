071220d2d7c40eacf422838b1bc50c4a842ab723
Malikov Denis <ryki.admirala@gmail.com>
2021-02-16 13:19:43 +0200

    disable logDebug fn, $patch

--- src/app/shared/helpers/debug.function.ts
+++ src/app/shared/helpers/debug.function.ts
@@ -1,4 +1,5 @@
 export const logDebug = (message: string, onlyServer: boolean = true) => {
+  return;
   if (onlyServer && typeof window !== 'undefined') { return; }
 
   console.log(`${new Date().toISOString()} - ${message}`);
