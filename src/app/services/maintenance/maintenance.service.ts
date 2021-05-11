e3baa5b188037c888e5b68e524ed6a69aaf046f8
Malikov Denis <ryki.admirala@gmail.com>
2021-05-11 23:52:14 +0300

    [KL-525] add MaintenanceInfoDto

--- src/app/services/maintenance/maintenance.service.ts
+++ src/app/services/maintenance/maintenance.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
+import { ResponseDto } from '../../shared/dtos/response.dto';
+import { MaintenanceInfoDto } from '../../shared/dtos/maintenance-info.dto';
+import { API_HOST } from '../../shared/constants';
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +12,17 @@ import { map, tap } from 'rxjs/operators';
 export class MaintenanceService {
 
   private isMaintenanceInProgress: boolean = false;
-  private maintenanceEndTime: Date = null;
+  private maintenanceEndTime: Date | null = null;
 
   constructor(private http: HttpClient) { }
 
   setMaintenanceInfo(): Observable<boolean> {
-    return this.http.get('')
+    return this.http.get<ResponseDto<MaintenanceInfoDto>>(`${API_HOST}/api/v1/maintenance`)
       .pipe(
         tap(response => {
-          this.isMaintenanceInProgress = response.isMaintenanceInProgress;
-          this.maintenanceEndTime = new Date(response.maintenanceEndTime);
+          this.isMaintenanceInProgress = response.data.isMaintenanceInProgress;
+          const endTime = response.data.maintenanceEndTime;
+          this.maintenanceEndTime = endTime ? new Date(endTime) : null;
         }),
         map(response => true)
       );
